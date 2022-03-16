2f9eefcc5533de75ca88a1160871774a209f3f51
Eugene Charniauski <yauheni.charniauski@gmail.com>
2022-03-16 20:27:03 +0300

    Update update-all message

--- pull-checkout-remotes-upstream-staging.sh
+++ pull-checkout-remotes-upstream-staging.sh
@@ -9,12 +9,12 @@ upstream=$(git remote get-url upstream 2> /dev/null)
 
 if [[ ! -z $origin && ! -z $upstream ]]; then
   cur_branch=$(git rev-parse --abbrev-ref HEAD)
-  echo -e "${PURPLE}${PWD##*/} FETCH MERGE (${cur_branch})${NC}"
   if [[ $cur_branch != 'staging' ]]; then
     echo -e "${CYAN}"
     git checkout staging
     echo -e "${NC}"
   fi
+  echo -e "${PURPLE}${PWD##*/} PULL upstream/staging HARD RESET origin/staging${NC}"
   git checkout staging
   git pull upstream staging
   git reset staging --hard
