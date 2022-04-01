b61feabedb8251110cca5d4ba4af63fe04c29ffe
Eugene Charniauski <yauheni.charniauski@gmail.com>
2022-04-01 13:48:39 +0300

    Rename  pull-checkout-remotes-upstream-staging command

--- all-update.sh
+++ all-update.sh
@@ -7,16 +7,18 @@ BFX_ROOT_REPO=$PWD
 echo $BFX_ROOT_REPO
 
 ~/cmd/create-upstream.sh
-~/cmd/pull-checkout-remotes-upstream-staging.sh
+~/cmd/update.sh
 
 for lib in ./src/local_modules/*; do
   cd $lib
 
     ~/cmd/create-upstream.sh
-    ~/cmd/pull-checkout-remotes-upstream-staging.sh
+    ~/cmd/update.sh
 
   cd $BFX_ROOT_REPO
 done
+
+~/cmd/all-status.sh
 
 
 
