37addfbf591c71c43d003fc55ca6fe6017ae296d
Eugene Charniauski <yauheni.charniauski@gmail.com>
2023-07-05 14:10:49 +0800

    Add pear commands

---
+++ pear-two.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' 
+
+NODE_VERSION="v18.15.0"
+
+STORE="~/keet/store/${NODE_VERSION}/${1}"
+
+unset NODE_OPTIONS
+source ~/.nvm/nvm.sh
+nvm use $NODE_VERSION
+echo -e "${PURPLE}store: ${STORE}${NC}"
+npm run dev -- -s $STORE
